c6732dde42db39c178a1b0bbed41134cd2e44fb0
0xstacker <oladotuntemitopeakorede@gmail.com>
2025-08-02 00:11:00 +0100

    update deployment script

--- script/Fungily.s.sol
+++ script/Fungily.s.sol
@@ -8,12 +8,10 @@ import {FungilyCollectionFactory} from "../src/Fungily-NFTs-Launchpad/Fungily.so
 contract Deploy is Script {
     function run() public {
         address initialFeeReceiver = 0x7cC8F5d78acfaa517eA62C5C579241eD9E1D190b; // Replace with actual address
-        uint256 initialPlatformMintFee = 0.0005 ether; // Example mint fee
-        uint16 initialPlatformSalesFeeBps = 500; // Example sales fee in basis points (2.5%)
 
+        uint256 platformMintFee = 0.0005 ether; // Example mint fee
         vm.startBroadcast();
-        FungilyCollectionFactory factory =
-            new FungilyCollectionFactory(initialFeeReceiver, initialPlatformMintFee, initialPlatformSalesFeeBps);
+        FungilyCollectionFactory factory = new FungilyCollectionFactory(initialFeeReceiver);
 
         vm.stopBroadcast();
     }
